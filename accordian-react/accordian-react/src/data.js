dd279f56fe4d293b8287a9fb081311ddedf39d37
ArhaanAli04 <arhaan.ali2004@gmail.com>
2024-08-24 18:26:54 +0530

    react

--- accordian-react/accordian-react/src/data.js
+++ accordian-react/accordian-react/src/data.js
@@ -1,26 +1,26 @@
 const data=[
     {
         id:'1',
-        question:'who is the Formula 1 current world champion?',
-        answer: 'Max Verstappen',
+        question:'Who is the Formula 1 current world champion?',
+        answer: 'Max Verstappen is the current World Champion',
     
     },
     {
         id:'2',
-        question:'who has the most World titles in Formula 1?',
+        question:'Who has the most World titles in Formula 1?',
         answer: 'Michael Schumacher and Lewis Hamilton',
     
     },
     {
         id:'3',
-        question:'who is the underrated Goat of Formula 1?',
+        question:'Who is the underrated Goat of Formula 1?',
         answer: 'Logan Sargent',
     
     },
     {
         id:'4',
-        question:'who is leading the 2024 drivers championship?',
-        answer: 'Max Verstappen',
+        question:'Who is leading the 2024 drivers championship?',
+        answer: 'Max Verstappen leads by 78 points to Lando Norris',
     
     },
     
