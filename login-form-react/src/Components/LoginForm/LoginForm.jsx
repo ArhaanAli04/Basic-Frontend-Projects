a5708711cdbb802983b153427fff2e2cd48f2da4
ArhaanAli04 <arhaan.ali2004@gmail.com>
2024-09-26 14:52:45 +0530

    react

--- login-form-react/src/Components/LoginForm/LoginForm.jsx
+++ login-form-react/src/Components/LoginForm/LoginForm.jsx
@@ -1,6 +1,6 @@
 // LoginForm.jsx
 
-import React from 'react';
+
 import { FaUser, FaLock } from 'react-icons/fa';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './LoginForm.css';
--- nav-att-assign-react/src/Components/Navbar.jsx
+++ nav-att-assign-react/src/Components/Navbar.jsx
@@ -1,14 +1,15 @@
 // src/Components/Navbar.jsx
+
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // Optional: Create a CSS file for styling the navbar
+import './Navbar.css'; // Import the CSS file for styling
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <ul className="navbar-links">
+    <nav className="navbar"> {/* Apply CSS to the navbar */}
+      <ul className="navbar-links"> {/* CSS targets ul elements */}
         <li>
-          <Link to="/home">Home</Link>
+          <Link to="/home">Home</Link> {/* Link component for navigation */}
         </li>
         <li>
           <Link to="/attendance">Attendance</Link>
